81039c5c2e890d542298dcc3657168bf6162db6a
Oskar <oskarwylucki@gmail.com>
2022-05-07 14:14:03 +0200

    Login added

--- base/views.py
+++ base/views.py
@@ -2,9 +2,37 @@ from django.shortcuts import render,redirect
 from .models import Room, Topic
 from django.db.models import Q
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password = password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Username or password does not exist')
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
 
 def index(request):
     q=request.GET.get('q') if request.GET.get('q') != None else ''
@@ -21,6 +49,7 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html',context)
 
+# @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
